1d66a8c0b4aeaf8d2b00d038abfd03354a5024d9
davidRiv657 <97049317+davidRiv657@users.noreply.github.com>
2022-01-05 12:06:32 -0600

    Updates~David

--- fakebook.py
+++ fakebook.py
@@ -1,7 +1,49 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '4889e1762a1993c7ed696c468baa5c34'
+
+posts = [
+    {
+        'author': 'David Rivas',
+        'title': 'User Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 3rd, 2022'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'User Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 4th, 2022'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+def home():
+    return render_template('Home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('About.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
